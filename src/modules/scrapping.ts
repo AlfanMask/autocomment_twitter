87a92ec6a53436e47204ed8041e618c34cf5221b
AlfanMask <afanh123123@gmail.com>
2024-08-16 10:32:44 +0700

    fix: skip if post contains image or videos

--- src/modules/scrapping.ts
+++ src/modules/scrapping.ts
@@ -11,18 +11,29 @@ export default async function scrapping() {
     let threadMsgText: string = '';
     await delay(2000)
     for (const article of articleElements) {
+      // only get if article is not contains image/video
+      const articleHTML = await article.evaluate(el => el.innerHTML);
       console.log('scrapping-2')
-      const textContent = await article.evaluate(el => el.textContent);
+      if (articleHTML?.includes('<div aria-label="Image"') || articleHTML?.includes('<div data-testid="videoComponent"')) {
+        console.log("CONTAINS IMAGE/VIDEO! Will rescrape...")
+        await goHome()
+        await delay(10000)
+        await scrapping()
+        return;
+      }
+
       console.log('scrapping-3')
+      const textContent = await article.evaluate(el => el.textContent);
+      console.log('scrapping-4')
       if (!textContent?.includes(`@${process.env.USERNAME_TWT}`)) {
-        console.log('scrapping-4')
-        await article?.click()
         console.log('scrapping-5')
+        await article?.click()
+        console.log('scrapping-6')
         await delay(2000)
         threadMsgText = await page.evaluate(() => {
             return document.querySelector('div[data-testid="tweetText"] span')?.innerHTML || '';
         })
-        console.log('scrapping-6')
+        console.log('scrapping-7')
         await delay(2000)
         break;
       }
@@ -31,16 +42,16 @@ export default async function scrapping() {
     // answer the thread
     await comment(threadMsgText)
     await delay(3000)
-    console.log('scrapping-7: waiting for 10 minutes to repeat the process')
+    console.log('scrapping-8: waiting for 10 minutes to repeat the process')
 
     // if any popup with close button -> click close
     const buttonCount = await page.$$eval('button[aria-label="Close"]', (buttons) => buttons.length);
-    console.log('scrapping-8')
+    console.log('scrapping-9')
     const isAnyCloseBtn = buttonCount > 0;
     if (isAnyCloseBtn) {
         await page.locator('button[aria-label="Close"]').click()
         await delay(2000);
-        console.log('scrapping-9')
+        console.log('scrapping-10')
     }
 
     // wait for 10 minutes
@@ -48,10 +59,10 @@ export default async function scrapping() {
 
     // refresh to main page with newly freshed timeline
     await goHome()
-    console.log('scrapping-10')
+    console.log('scrapping-11')
 
     // repeat
-    console.log('scrapping-11: SCRAPPING FINISHED!')
+    console.log('scrapping-12: SCRAPPING FINISHED!')
     await delay(10000)
     await scrapping()
 }
